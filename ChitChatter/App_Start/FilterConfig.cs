475a17b1623cfc85facf17a3b8d6744d7ebfab75
KurtWeber <weberkrt@msn.com>
2016-10-31 13:39:15 -0400

    Added last section

--- ChitChatter/App_Start/FilterConfig.cs
+++ ChitChatter/App_Start/FilterConfig.cs
@@ -39,6 +39,16 @@ namespace ChitChatter
     }
 }
 
+
+public class FilterConfig
+{
+    public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+    {
+        filters.Add(new HandleErrorAttribute());
+        filters.Add(new RequireHttpsAttribute());
+    }
+}
+
 //using System.Web;
 //using System.Web.Mvc;
 
